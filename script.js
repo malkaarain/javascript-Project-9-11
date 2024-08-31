6fe499a59cdc0169c01da91b6e144c638965d9b3
Malka arain <malkaarain92@gmail.com>
2024-09-01 00:46:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+// Question no 1
+
+// let city = prompt("Enter the name of your city:");
+
+// if (city === "karachi") {
+//     alert("Welcome to the city of lights!");
+// } else {
+//     alert("Welcome to " + city);
+// }
+
+// Question no 2
+
+// let gender = prompt("Enter your gender :");
+
+// if (gender === "male") {
+//     alert("Good Morning Sir.");
+// } else if (gender === "female") {
+//     alert("Good Morning Ma’am.");
+// }
+
+// Question no 3
+
+// let color = prompt("Enter your road traffic signal :");
+
+// if (color === "red") {
+//     alert ("Must Stop") ;
+// } else if (color === "yellow") {
+//     alert ("Ready to move");
+// } else if (color === "green") {
+//     alert ("Move now");
+// }
+
+// Question no 4
+
+// let fuel = prompt(" fuel in your car :");
+// if (fuel < 0.25){
+//     alert ("please refill your fuel")
+// }
+
+// Question no 5
+
+// A : true
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// B : False
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// c
+// var c = 12;
+// if (++c < 14){
+//   alert("condition 3 is true");
+//   }
+
+  // D : true
+  // var materialCost = 20000;
+  // var laborCost = 2000;
+  // var totalCost = materialCost + laborCost;
+  // if (totalCost === laborCost + materialCost){
+  // alert("The cost equals");
+  // }
+
+    // E
+  
+  // if (true){
+  //   alert("True");
+  //   }
+  //   if (false){
+  //   alert("False");
+  //   }
+
+
+    //  F
+  //  if("car" < "cat"){
+  //   alert("car is smaller than cat");
+  //   }
+
+ 
